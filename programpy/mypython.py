41b07e1bd30e1496e97f4a7b5e0c23b57ed40fd9
Max Moulds <max@maxmoulds.org>
2017-07-31 11:21:30 -0700

    added some reading because

--- programpy/mypython.py
+++ programpy/mypython.py
@@ -6,13 +6,19 @@ def random_char(x):
 
 num_files = 3
 num_chars_in_files = 10
+file_names = []
 
 for x in range(0, num_files): #zero based still, aww snap
   random_chars = random_char(num_chars_in_files)
   #print(random_chars, " is random chars")
   with open((random_chars + '.txt'), "w") as f:
     f.write(random_chars + '\n')
-    print(random_chars),
+    file_names.append(random_chars + ".txt")
+
+for name in file_names:
+  with open(name, "r") as f:
+    for line in f:
+      print(line, end=''),
 
 ar = random.randint(1,42)
 br = random.randint(1,42)
