f558e47b1fb7f8a25d6e15986b1fc13138b09c96
Max Moulds <max@maxmoulds.org>
2017-08-17 03:33:42 -0700

    bedtime, almost there

--- otp/otp_enc.c
+++ otp/otp_enc.c
@@ -20,12 +20,14 @@
 #define DEBUG 1
 #define ERROR_PORT 2
 #define ERROR_BAD_CHAR 1
-#define MAX_READ 255
+#define MAX_READ 1023
 #define SPACE " "
 #define CHARSET_NUM 27
 #define ASCII_OFFSET 64
-#define OTP_ENC_INT 5 //testing should be 1
+#define OTP_ENC_INT 1 //testing should be 1
+#define OTP_ENC_KEYFILE_INT 11
 #define OTP_ENC_D_INT 2
+#define OTP_ENC_D_KEYFILE_RESPONSE_INT 21
 #define OTP_DEC_INT 3
 #define OTP_DEC_D_INT 4
 
@@ -50,6 +52,7 @@ int otp_enc(char * input_filename, char * key_filename, int port) {
   //check file lengths
   FILE * fp;
   FILE * kp;
+  FILE * cp; 
   int input_file_size = 0;
   int key_file_size = 0;
   if (!(fp = fopen(input_filename, "r"))) 
@@ -76,12 +79,16 @@ int otp_enc(char * input_filename, char * key_filename, int port) {
     fprintf(stderr, "Error: key file size %d is smaller than input file \'%s\' size %d", key_file_size, input_filename, input_file_size);
     exit(ERROR_BAD_CHAR);
   }
-  fclose(kp);
+  //fclose(kp);
   int socketFD, portNumber, charsWritten, charsRead;
   struct sockaddr_in serverAddress;
   struct hostent* serverHostInfo;
   //char * buffer = calloc(input_file_size+10, sizeof(char));
-  
+  fd_set set;
+  int result;
+  struct timeval tv;
+  tv.tv_sec = 5;
+  tv.tv_usec = 100 * 1000;
 
   char message[MAX_READ+1+(sizeof(int)/sizeof(char))+1];
   memset(message, -1, sizeof(message));
@@ -91,8 +98,9 @@ int otp_enc(char * input_filename, char * key_filename, int port) {
   buffer[MAX_READ] = '\0';
   //buffer = (((int)buffer)+1);
   //printf("buffer + sizeof(int)/sizeof(char) : %lu : \n", (sizeof(int)/sizeof(char)));
-  int * id = message;
+  int * id = (void*)message;
   *id = OTP_ENC_INT;
+  int keyfile_sent = 0;
   //buffer += (sizeof(int)/sizeof(char));//u kno structs are cooler. but so are floating points...
   //*id = 10; 
   //printf("id is %d\n *buffer[0] = %p, *buffer[MAX_READ] = %p, message = %lu\n", *id, buffer, (&(buffer[MAX_READ-1])), sizeof(message)); 
@@ -118,24 +126,85 @@ int otp_enc(char * input_filename, char * key_filename, int port) {
   // Connect to server
   if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
     error("CLIENT: ERROR connecting", ERROR_PORT);
-  //while (!feof(fp))
-  //{
-  memset(buffer, '\0', MAX_READ); // Clear out the buffer array
-  if (fgets(buffer, MAX_READ - 1, fp) != NULL)  //stdin); // Get input from the user, trunc to buffer - 1 chars, leaving \0
+  //set cp to fp first... idk why. 
+  cp = fp;
+  result = 1;
+SEND:
+  if (keyfile_sent == 0)
   {
-    buffer[strcspn(buffer, "\n")] = '\0'; // Remove the trailing \n that fgets adds
-    charsWritten = send(socketFD, message, sizeof(message), 0);
-    if (charsWritten < 0) error("CLIENT: ERROR writing to socket", ERROR_PORT);
-    if (charsWritten < sizeof(message)) printf("CLIENT: WARNING: Not all data written to socket!\n");
-    // Get return message from server
-    memset(buffer, '\0', MAX_READ); // Clear out the buffer again for reuse
-    charsRead = recv(socketFD, message, sizeof(message), 0);
-    if (charsRead < 0) error("CLIENT: ERROR reading from socket", ERROR_PORT);
-    printf("CLIENT: I (%d) received this from the server: \"%s\"\n", *id, buffer);
+    cp = kp;
   }
-  //}
+  while (!feof(cp))
+  {
+//    do {
+//      FD_ZERO (&set);
+//      FD_SET (socketFD, &set);
+//      result = select(FD_SETSIZE, &set, NULL, NULL, &tv);
+//      //result = select(cp + 1, &set, NULL, NULL, NULL);
+//    }
+//    while (result == -1 && errno == EINTR);
+
+    if (result == -1) {
+      error("CLIENT: error in select", ERROR_PORT); // error occurred in select()
+    } else if (result == 0) {
+      error("CLIENT: Timeout occurred!  No data after 10.5 seconds.", ERROR_PORT);
+    } else {
+
+      if (keyfile_sent == 0)
+      {
+        //do now...
+        //cp = kp;
+        *id = OTP_ENC_KEYFILE_INT;
+      }
+      else 
+      {
+        //cp = fp;
+        *id = OTP_ENC_INT;
+      }
+      memset(buffer, '\0', MAX_READ); // Clear out the buffer array
+      if (fgets(buffer, MAX_READ - 1, cp) != NULL)  //stdin); // Get input from the user, trunc to buffer - 1 chars, leaving \0
+      {
+        buffer[strcspn(buffer, "\n")] = '\0'; // Remove the trailing \n that fgets adds
+        printf("CLIENT: read - :: %s ::\n", buffer);
+        //sleep(1);
+        charsWritten = send(socketFD, message, sizeof(message), 0);
+        if (charsWritten < 0) {
+          fprintf(stderr, "Err...%s", strerror(errno));
+          error("CLIENT: ERROR writing to socket", ERROR_PORT);
+
+        }
+          if (charsWritten < sizeof(message)) printf("CLIENT: WARNING: Not all data written to socket!\n");
+        // Get return message from server
+        memset(buffer, '\0', MAX_READ); // Clear out the buffer again for reuse
+        charsRead = recv(socketFD, message, sizeof(message), 0);
+        if (charsRead < 0) {
+          //error("CLIENT: ERROR reading from socket", ERROR_PORT);
+          fprintf(stderr, "Err...%s", strerror(errno));
+        error("CLIENT: ERROR reading from socket", ERROR_PORT);
+        }
+          printf("CLIENT: I (%d) received this from the server: \"%s\"\n", *id, buffer);
+        //sleep(1);
+      }
+    }
+       do {
+      FD_ZERO (&set);
+      FD_SET (socketFD, &set);
+      result = select(FD_SETSIZE, NULL, &set, NULL, &tv);
+      //result = select(cp + 1, &set, NULL, NULL, NULL);
+    }   
+    while (result == -1 && errno == EINTR);
+ 
+  }
+  if (keyfile_sent == 0)
+  {
+    printf("CLIENT: keyfile transferred\n");
+    keyfile_sent = 1;
+    cp = fp;
+    goto SEND;
+  }
   close(socketFD); // Close the socket
   fclose(fp);
+  fclose(kp);
   return 0;
 }
 
--- otp/otp_enc_d.c
+++ otp/otp_enc_d.c
@@ -1,4 +1,5 @@
 //
+//TODO: send keyfile. 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,6 +19,8 @@
 #define ASCII_OFFSET 64
 #define OTP_ENC_INT 1
 #define OTP_ENC_D_INT 2
+#define OTP_ENC_KEYFILE_INT 11
+#define OTP_ENC_D_KEYFILE_RESPONSE_INT 21
 
 #define HOSTNAME "localhost"
 
@@ -40,8 +43,8 @@ int otp_enc_d(int port) {
   int listenSocketFD, establishedConnectionFD, portNumber, charsRead;
   socklen_t sizeOfClientInfo;
 
-  char keybuf[MAX_READ+1];
-  memset(keybuf, -1, sizeof(keybuf));
+  //char keybuf[MAX_READ+1];
+  char * keybuf = calloc(MAX_READ+1, sizeof(char));
   keybuf[MAX_READ] = '\0';
   char ciphertext[MAX_READ+1];
   memset(ciphertext, -1, sizeof(ciphertext));
@@ -54,20 +57,20 @@ int otp_enc_d(int port) {
   //buffer[sizeof(buffer)-1] = NULL; //redundant if all is good but doesnt hurt. 
   buffer[MAX_READ] = '\0';//oo ima bad boy
   //printf("buffer + sizeof(int)/sizeof(char) : %lu : \n", (sizeof(int)/sizeof(char)));
-  int * id = response;
+  int * id = (void*)response;
 
   //buffer += (sizeof(int)/sizeof(char));//u kno structs are cooler. but so are floating points...
   *id = 10; 
   printf("id is %d\n *buffer[0] = %p, *buffer[MAX_READ] = %p, message = %lu, keybuf = %lu, and ciphertext = %lu\n", *id, buffer, (&(buffer[MAX_READ-1])), sizeof(response), sizeof(keybuf), sizeof(ciphertext));
-  printf("CHECK:  %x (hex), %lu, should be %d\n", ((&(buffer[MAX_READ-1]))-(&(buffer[0]))), ((&(buffer[MAX_READ-1]))-(((buffer)))), MAX_READ);
-  printf("CHECK2:  %x (dhex), %lu, should be %d\n", ((&(response[MAX_READ+2+(sizeof(int)/sizeof(char))]))-(&(response[0]))), ((&(response[MAX_READ+2+(sizeof(int)/sizeof(char))]))-(((response)))), MAX_READ+2+(sizeof(int)/sizeof(char)+1));
+  printf("CHECK:  %lx (hex), %lu, should be %d\n", ((&(buffer[MAX_READ-1]))-(&(buffer[0]))), ((&(buffer[MAX_READ-1]))-(((buffer)))), MAX_READ);
+  printf("CHECK2:  %lx (dhex), %lu, should be %lu\n", ((&(response[MAX_READ+2+(sizeof(int)/sizeof(char))]))-(&(response[0]))), ((&(response[MAX_READ+2+(sizeof(int)/sizeof(char))]))-(((response)))), MAX_READ+2+(sizeof(int)/sizeof(char)+1));
   //char ciphertext[MAX_READ+1];
-  printf("main = %p, id = %p, response = %p , response[max-read+dfs] = %p, sizeofid = %d,\n", main, id, response, &(response[MAX_READ+2+(sizeof(int)/sizeof(char))]), sizeof(*id)); 
-  printf("strlent buffer = %d,sizeof buffer = %d,  buffer[0] = %d, buffer[sizeofbuffer-1] = %d, buffer[maxread[ = %d ,\n", strlen(buffer), sizeof(buffer), buffer[0], buffer[sizeof(buffer)-1], buffer[MAX_READ-1]);
+  printf("main = %p, id = %p, response = %p , response[max-read+dfs] = %p, sizeofid = %lu,\n", main, id, response, &(response[MAX_READ+2+(sizeof(int)/sizeof(char))]), sizeof(*id)); 
+  printf("strlent buffer = %lu,sizeof buffer = %lu,  buffer[0] = %d, buffer[sizeofbuffer-1] = %d, buffer[maxread[ = %d ,\n", strlen(buffer), sizeof(buffer), buffer[0], buffer[sizeof(buffer)-1], buffer[MAX_READ-1]);
 
 
   struct sockaddr_in serverAddress, clientAddress;
-  FILE * kp;
+  //FILE * kp;
   //if (argc < 2) { fprintf(stderr,"USAGE: %s port\n", argv[0]); exit(1); } // Check usage & args
 
   // Set up the address struct for this process (the server)
@@ -89,69 +92,80 @@ int otp_enc_d(int port) {
   {
     error("ERROR on listen", ERROR_PORT);
   }
-  kp = fopen("keyfile2", "r");
+  //kp = fopen("keyfile2", "r");
   //char * keybuf = calloc(MAX_READ+1, sizeof(char));
   int encrypted_chars = 0;
   ssize_t total = 0;
+  int keybuf_chars = 0;
 
-  if (fgets(keybuf, sizeof(keybuf)-1, kp) != NULL)
-  {
-    while(1) {
-      // Accept a connection, blocking if one is not available until one connects
-      sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect
-      establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
-      if (establishedConnectionFD < 0) error("ERROR on accept", ERROR_PORT);
-      printf("SERVER: Connected Client at port %d\n", ntohs(clientAddress.sin_port));
-      encrypted_chars = 0;
-      memset(buffer, '\0', MAX_READ+1);
-      memset(ciphertext, '\0', sizeof(ciphertext));
-      (charsRead = recv(establishedConnectionFD, response, sizeof(response), 0) > 0); // Read the client's message from the socket
-      if (charsRead < 0) error("ERROR reading from socket", ERROR_PORT);
-      printf("SERVER: I received this from the client(%d): \"%s\"\n", *id, buffer);
-      if ((*id) != OTP_ENC_INT) 
-      {
-        //do i want to exit or just log and keep accepting. hmmm. lets do both
-        fprintf(stderr, "ERROR: rejected connection from %d - not otp_enc trying to connect\n", *id);
-        //close fd?
-        snprintf(ciphertext, 61, "ERROR: rejected connection - not otp_enc trying to connect");
-        *id = ERROR;
-        //continue;
-        //error("ERROR: rejected - not otp_enc trying to connect", ERROR_PORT);
-      }
-      else 
-      {
-        total += charsRead;
-        *id = OTP_ENC_D_INT;
-        encrypted_chars += encrypt(buffer, strlen(buffer), keybuf, strlen(keybuf), ciphertext, sizeof(ciphertext));
-        printf("SERVER: Encrypted text is : %s :\n", ciphertext);
-      }
-      // Send a Success message back to the client
-      memset(buffer, '\0', MAX_READ);
-      memcpy(buffer, ciphertext, MAX_READ);
-
-      printf("SERVER: sending :: %d - %s\n", *id, buffer);
-      //snprintf(buffer, MAX_READ-1, "I am the server, and I got your WHOLE message of %zd chars", encrypted_chars);
-      charsRead = send(establishedConnectionFD, response, sizeof(response), 0); // Send success back
-      if (charsRead < 0) error("ERROR writing to socket", ERROR_PORT);
-      //memset(buffer, '\0', sizeof(buffer)); //ready buffer for next message
-      //memset(ciphertext, '\0', sizeof(ciphertext));
-      //snprintf(buffer, sizeof(buffer)-1, "I am the server, and I got your WHOLE message of %zd chars", total);
-      //charsRead = send(establishedConnectionFD, buffer, strlen(buffer), 0); // Send success back
-
-      close(establishedConnectionFD); // Close the existing socket which is connected to the client
-    }
-  }
-
-  close(listenSocketFD); // Close the listening socket
-  fclose(kp);
-  //}
-  //else 
+  //if (fgets(keybuf, sizeof(keybuf)-1, kp) != NULL)
   //{
-  //problem getting keyfile
-  // error("problem with keyfile", ERROR_BAD_CHAR);
-  //}
-  return 0;
+  while(1) {
+    // Accept a connection, blocking if one is not available until one connects
+    sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect
+    establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
+    if (establishedConnectionFD < 0) error("ERROR on accept", ERROR_PORT);
+    printf("SERVER: Connected Client at port %d\n", ntohs(clientAddress.sin_port));
+    encrypted_chars = 0;
+    memset(buffer, '\0', MAX_READ+1);
+    memset(ciphertext, '\0', sizeof(ciphertext));
+    (charsRead = recv(establishedConnectionFD, response, sizeof(response), 0) > 0); // Read the client's message from the socket
+    if (charsRead < 0) error("ERROR reading from socket", ERROR_PORT);
+    printf("SERVER: I received this from the client(%d): \"%s\"\n", *id, buffer);
+    if ((*id) == OTP_ENC_KEYFILE_INT)
+    {
+      //read into keybuf.?
+      if (keybuf_chars == 0) 
+      {
+        //first time
+        memcpy(keybuf, buffer, MAX_READ);
+        memcpy(ciphertext, buffer, MAX_READ);
+        //*id = OTP_ENC_D_KEYFILE_RESPONSE_INT;
+        //cleanup
+      }
+      else
+      {
+        //extend the keybuf...
+        keybuf = realloc(keybuf, strlen(buffer)+1);
+        strcat(keybuf, buffer);//eww, maybe strncat...
+        memcpy(ciphertext, buffer, MAX_READ);
+      }
+      *id = OTP_ENC_D_KEYFILE_RESPONSE_INT;
+    }
+    else if ((*id) != OTP_ENC_INT) 
+    {
+      //do i want to exit or just log and keep accepting. hmmm. lets do both
+      fprintf(stderr, "ERROR: rejected connection from %d - not otp_enc trying to connect\n", *id);
+      //close fd?
+      snprintf(ciphertext, 61, "ERROR: rejected connection - not otp_enc trying to connect");
+      *id = ERROR;
+      //continue;
+      //error("ERROR: rejected - not otp_enc trying to connect", ERROR_PORT);
+    }
+    else 
+    {
+      total += charsRead;
+      *id = OTP_ENC_D_INT;
+      encrypted_chars += encrypt(buffer, strlen(buffer), keybuf, strlen(keybuf), ciphertext, sizeof(ciphertext));
+      printf("SERVER: Encrypted text is : %s :\n", ciphertext);
+    }
+    // Send a Success message back to the client
+    memset(buffer, '\0', MAX_READ);
+    memcpy(buffer, ciphertext, MAX_READ);
+    printf("SERVER: sending :: %d - %s\n", *id, buffer);
+    //snprintf(buffer, MAX_READ-1, "I am the server, and I got your WHOLE message of %zd chars", encrypted_chars);
+    charsRead = send(establishedConnectionFD, response, sizeof(response), 0); // Send success back
+    if (charsRead < 0) error("ERROR writing to socket", ERROR_PORT);
+    //memset(buffer, '\0', sizeof(buffer)); //ready buffer for next message
+    //memset(ciphertext, '\0', sizeof(ciphertext));
+    //snprintf(buffer, sizeof(buffer)-1, "I am the server, and I got your WHOLE message of %zd chars", total);
+    //charsRead = send(establishedConnectionFD, buffer, strlen(buffer), 0); // Send success back
+    close(establishedConnectionFD); // Close the existing socket which is connected to the client
   }
+  close(listenSocketFD); // Close the listening socket
+  free(keybuf);
+  return 0;
+}
 
 int encrypt(char * message, int message_length,  char * key, int key_length, char * ciphertext, int ciphertext_length) {
   int i = 0;
